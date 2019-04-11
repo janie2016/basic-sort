310c5632ae015261b1b36b1be35b50464ce6ec3d
janie <1612116130@qq.com>
2019-04-11 14:43:36 +0800

    vfdvdf

---
+++ basicsort/src/binary/complete/classic_nonrecursive_traversal_preorder/TreeNode.java
@@ -0,0 +1,8 @@
+package binary.complete.classic_nonrecursive_traversal_preorder;
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
---
+++ basicsort/src/sort/basic/insertion/Main.java
@@ -0,0 +1,24 @@
+package sort.basic.insertion;
+
+import java.util.Arrays;
+
+import sort.basic.advanced.SortTestHelper;
+
+public class Main {
+
+    // 比较SelectionSort和InsertionSort两种排序算法的性能效率
+    // 此时，插入排序比选择排序性能略低
+    public static void main(String[] args) {
+
+        int N = 20000;
+        System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
+
+        Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.SelectionSort", arr1);
+        SortTestHelper.testSort("bobo.algo.InsertionSort", arr2);
+
+        return;
+    }
+}
